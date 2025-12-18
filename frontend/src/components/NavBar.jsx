87dbd23539469d05b60fedfcbf49bc6b1248d884
TyPurp <ltypurple0099@gmail.com>
2025-12-18 19:05:21 +0800

    updated of narbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -134,7 +134,7 @@ export default function NavBar({ children }) {
             <Link to="/admin/events" style={linkStyle}>Manage Events</Link>
             <Link to="/admin/users" style={linkStyle}>Manage Users</Link>
             <Link to="/locations" style={linkStyle}>Locations</Link>
-            <Link to="/event-list" style={linkStyle}>Event List</Link>
+            <Link to="/event-list" style={linkStyle}>Event</Link>
             <Link to="/map" style={linkStyle}>Map</Link>
             <Link to="/favourites" style={linkStyle}>Favourites</Link>
           </div>
